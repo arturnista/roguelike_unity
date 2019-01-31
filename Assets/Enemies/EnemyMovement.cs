4c870bb69af1c105892c4f3a8bfdd3cb666a8a05
Artur Morelle Nista <arturnista@gmail.com>
2019-01-30 22:53:02 -0200

    Enemy movement

--- Assets/Enemies/EnemyMovement.cs
+++ Assets/Enemies/EnemyMovement.cs
@@ -11,21 +11,79 @@ public class EnemyMovement : UnitMovement
     public TransformReference Target;
     [SerializeField]
     public FloatReference AttackRange;
+    [SerializeField]
+    public LayerMaskReference ObstacleLayer;
 #endregion
+
+    private Vector3 originalPosition;
+    private Vector3 targetPosition;
+    private Vector3 offsetPosition;
+
+    private Coroutine changeDirectionCoroutine;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        originalPosition = transform.position;
+        MoveSpeed.Value *= .3f;
+
+        changeDirectionCoroutine = StartCoroutine(ChangeDirection());
+    }
+
+    void OnEnable()
+    {
+        Target.OnChangeValue += OnChangeTarget;        
+    }
+
+    void OnDisable()
+    {
+        Target.OnChangeValue -= OnChangeTarget;        
+    }
 
     protected override void Update()
     {
         base.Update();
+        Vector2 targetDirection;
 
-        Direction.Value = Vector2.zero;
         if(Target.Value)
         {
-            float distance = Vector2.Distance(Target.Value.position, transform.position);
-            if(distance <= AttackRange.Value) return;
-
-            Vector2 targetDirection = Target.Value.position - transform.position;
-            Direction.Value = targetDirection.normalized;
+            Vector3 targetPosition = Target.Value.position + offsetPosition;
+            targetDirection = targetPosition - transform.position;
         }
+        else
+        {
+            Vector3 targetPosition = originalPosition + offsetPosition;
+            targetDirection = targetPosition - transform.position;
+        }
+
+        Direction.Value = targetDirection.normalized;
+    }
+
+    IEnumerator ChangeDirection()
+    {
+        while (true)
+        {
+            offsetPosition = (Vector3)Random.insideUnitCircle * 10f;
+            yield return new WaitForSeconds(Random.Range(1f, 3f));
+        }
+    }
+
+    IEnumerator ChangeTargetPosition()
+    {
+        while (true)
+        {
+            offsetPosition = (Vector3)Random.insideUnitCircle * 5f;
+            yield return new WaitForSeconds(Random.Range(8f, 12f));
+        }
+    }
+
+    void OnChangeTarget(Transform oldTarget)
+    {
+        StopCoroutine(changeDirectionCoroutine);
+        StartCoroutine(ChangeTargetPosition());
+
+        MoveSpeed.ResetValue();
     }
     
 }
